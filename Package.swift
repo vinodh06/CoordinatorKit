5db44ec73f2c5cfdefa89d4b75fa503903afc0c6
vinodh06 <vinodhkumarg@ymail.com>
2025-06-19 17:30:38 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,10 +13,6 @@ let package = Package(
             name: "CoordinatorKit",
             targets: ["CoordinatorKit"]
         ),
-        .executable(
-            name: "CoordinatorKitClient",
-            targets: ["CoordinatorKitClient"]
-        ),
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
@@ -35,9 +31,6 @@ let package = Package(
 
         // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "CoordinatorKit", dependencies: ["CoordinatorKitMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "CoordinatorKitClient", dependencies: ["CoordinatorKit"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
